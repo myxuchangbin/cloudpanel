57421b094f376cc280b7125cb966697b9e93a959
myxuchangbin <17875554+myxuchangbin@users.noreply.github.com>
2025-11-25 12:31:02 +0800

    Update droplet creation parameters and images

--- libs/do.py
+++ libs/do.py
@@ -80,7 +80,7 @@ class DoApi():
         return True
 
     # 创建实例
-    def create_droplet(self, name='panel', password='admin7788==', count=1, region='sfo3', size='s-1vcpu-1gb', image='centos-7-x64'):
+    def create_droplet(self, name='cloudpanel', password='admin7788==', count=1, region='sgp1', size='s-1vcpu-1gb', image='debian-12-x64'):
         try:
             url = f"{self.api_url}/v2/droplets"
 
@@ -88,6 +88,7 @@ class DoApi():
                 "names": [name] * int(count),
                 # "name": name,
                 # "user_data": USER_DATA.replace('admin7788==', password),
+                "user_data": f"#!/bin/bash\n echo 'root:{password}' | chpasswd",
                 "region": region,
                 "size": size,
                 "image": image,
@@ -127,12 +128,20 @@ class DoApi():
     @classmethod
     def get_images(cls):
         image_list = [
-            'centos-7-x64',
-            'centos-8-x64',
-            'debian-9-x64',
-            'debian-10-x64',
-            'ubuntu-18-04-x64',
-            'ubuntu-20-10-x64'
+            'debian-12-x64',
+            'debian-13-x64',
+            'centos-stream-9-x64',
+            'centos-stream-10-x64',
+            'rockylinux-8-x64',
+            'rockylinux-9-x64',
+            'rockylinux-10-x64',
+            'almalinux-8-x64',
+            'almalinux-9-x64',
+            'almalinux-10-x64',
+            'ubuntu-22-04-x64',
+            'ubuntu-24-04-x64',
+            'ubuntu-25-04-x64',
+            'ubuntu-25-10-x64'
         ]
         return image_list
 
@@ -158,51 +167,75 @@ class DoApi():
     @classmethod
     def get_region_map(cls):
         data_list = [
-            ('nyc1', '美国-纽约'),
-            ('sfo3', '美国-旧金山'),
-            ('ams3', '荷兰-阿姆斯特丹'),
-            ('sgp1', '亚太-新加坡'),
-            ('lon1', '英国-伦敦'),
-            ('fra1', '德国-法兰克福'),
-            ('tor1', '加拿大-多伦多'),
-            ('blr1', '印度-班加罗尔')
+            ('nyc1', '美国-纽约1'),
+            ('nyc2', '美国-纽约2'),
+            ('nyc3', '美国-纽约3'),
+            ('sfo1', '美国-旧金山1'),
+            ('sfo2', '美国-旧金山2'),
+            ('sfo3', '美国-旧金山3'),
+            ('atl1', '美国-亚特兰大1'),
+            ('ams2', '荷兰-阿姆斯特丹2'),
+            ('ams3', '荷兰-阿姆斯特丹3'),
+            ('sgp1', '亚太-新加坡1'),
+            ('lon1', '英国-伦敦1'),
+            ('fra1', '德国-法兰克福1'),
+            ('tor1', '加拿大-多伦多1'),
+            ('syd1', '澳大利亚-悉尼1'),
+            ('blr1', '印度-班加罗尔1')
         ]
         return data_list
 
     @classmethod
     def get_region_dist(cls):
         region_dist = {
-            'nyc1': '美国-纽约',
-            'sfo3': '美国-旧金山',
-            'ams3': '荷兰-阿姆斯特丹',
-            'sgp1': '亚太-新加坡',
-            'lon1': '英国-伦敦',
-            'fra1': '德国-法兰克福',
-            'tor1': '加拿大-多伦多',
-            'blr1': '印度-班加罗尔'
+            'nyc1': '美国-纽约1',
+            'nyc2': '美国-纽约2',
+            'nyc3': '美国-纽约3',
+            'sfo1': '美国-旧金山1',
+            'sfo2': '美国-旧金山2',
+            'sfo3': '美国-旧金山3',
+            'atl1': '美国-亚特兰大1',
+            'ams2': '荷兰-阿姆斯特丹2',
+            'ams3': '荷兰-阿姆斯特丹3',
+            'sgp1': '亚太-新加坡1',
+            'lon1': '英国-伦敦1',
+            'fra1': '德国-法兰克福1',
+            'tor1': '加拿大-多伦多1',
+            'blr1': '印度-班加罗尔1',
+            'syd1': '澳大利亚-悉尼1'
         }
         return region_dist
 
     @classmethod
     def get_price_map(cls):
         data_list = [
-            ('s-1vcpu-1gb', '1H/1G/25G/1T-$5'),
-            ('s-1vcpu-2gb', '1H/2G/50G/2T-$10'),
-            ('s-2vcpu-2gb', '2H/2G/30G/3T-$15'),
-            ('s-2vcpu-4gb', '2H/4G/80G/4T-$20'),
-            ('s-4vcpu-8gb', '4H/8G/160G/5T-$40'),
-            ## INTEL
-            ('s-1vcpu-1gb-intel', '1H/1G/25G/1T-$6-intel'),
-            ('s-1vcpu-2gb-intel', '1H/2G/50G/2T-$12-intel'),
-            ('s-2vcpu-2gb-intel', '2H/2G/30G/3T-$18-intel'),
-            ('s-2vcpu-4gb-intel', '2H/4G/80G/4T-$24-intel'),
-            ('s-4vcpu-8gb-intel', '4H/8G/160G/5T-$48-intel'),
-            ## AMD
-            ('s-1vcpu-1gb-amd', '1H/1G/25G/1T-$6-amd'),
-            ('s-1vcpu-2gb-amd', '1H/2G/50G/2T-$12-amd'),
-            ('s-2vcpu-2gb-amd', '2H/2G/30G/3T-$18-amd'),
-            ('s-2vcpu-4gb-amd', '2H/4G/80G/4T-$24-amd'),
-            ('s-4vcpu-8gb-amd', '4H/8G/160G/5T-$48-amd'),
+            # Basic
+            ('s-1vcpu-512mb-10gb', '1H/0.5G/10G/0.5T-$4'),
+            ('s-1vcpu-1gb', '1H/1G/25G/1T-$6'),
+            ('s-1vcpu-2gb', '1H/2G/50G/2T-$12'),
+            ('s-2vcpu-2gb', '2H/2G/60G/3T-$18'),
+            ('s-2vcpu-4gb', '2H/4G/80G/4T-$24'),
+            ('s-4vcpu-8gb', '4H/8G/160G/5T-$48'),
+            # INTEL
+            ('s-1vcpu-1gb-intel', '1H/1G/25G/1T-$7-intel'),
+            ('s-1vcpu-1gb-35gb-intel', '1H/1G/35G/1T-$8-intel'),
+            ('s-1vcpu-2gb-intel', '1H/2G/50G/2T-$14-intel'),
+            ('s-1vcpu-2gb-70gb-intel', '1H/2G/70G/2T-$16-intel'),
+            ('s-2vcpu-2gb-intel', '2H/2G/60G/3T-$21-intel'),
+            ('s-2vcpu-2gb-90gb-intel', '2H/2G/90G/3T-$24-intel'),
+            ('s-2vcpu-4gb-intel', '2H/4G/80G/4T-$28-intel'),
+            ('s-2vcpu-4gb-120gb-intel', '2H/4G/120G/4T-$32-intel'),
+            ('s-4vcpu-8gb-intel', '4H/8G/160G/5T-$56-intel'),
+            ('s-4vcpu-8gb-240gb-intel', '4H/8G/240G/5T-$64-intel'),
+            ('s-4vcpu-16gb-320gb-intel', '4H/16G/320G/6T-$96-intel'),
+            # AMD
+            ('s-1vcpu-1gb-amd', '1H/1G/25G/1T-$7-amd'),
+            ('s-1vcpu-2gb-amd', '1H/2G/50G/2T-$14-amd'),
+            ('s-2vcpu-2gb-amd', '2H/2G/60G/3T-$21-amd'),
+            ('s-2vcpu-4gb-amd', '2H/4G/80G/4T-$28-amd'),
+            ('s-2vcpu-8gb-amd', '2H/8G/100G/5T-$42-amd'),
+            ('s-4vcpu-8gb-amd', '4H/8G/160G/5T-$56-amd'),
+            ('s-4vcpu-16gb-amd', '4H/16G/200G/6T-$84-amd'),
         ]
         return data_list
 
